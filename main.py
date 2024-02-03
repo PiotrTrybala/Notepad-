361340599ffa05eeb68adf92a30067a27bff760f
Piotr Trybała <trybalapiotr4@gmail.com>
2024-02-03 10:12:36 +0100

    added toolbar

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ class NotepadPPP(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.editor = QTextEdit()
+        self.setCentralWidget(self.editor)
+
+        self.createToolBar()
+
+        self.setWindowTitle("Notepad+++")
+        self.showMaximized()
+
+    def createToolBar(self):
+        toolbar = QToolBar()
+        self.addToolBar(toolbar)
+
 app = QApplication(sys.argv)
 window = NotepadPPP()
 window.show()
